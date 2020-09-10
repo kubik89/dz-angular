4b2b331f92884bc142ab6dabf4b09493d4aa7967
Volodymyr <kubik89@yahoo.com>
2020-09-10 13:57:03 +0300

    DZ-1-Angular-Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {ToDoService} from './to-do.service';
+import {todolist} from './todo.models';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dz-angLess1';
+  todo: todolist[] = [];
+  constructor(private td: ToDoService) {
+    td.getToDoList().subscribe(jsonTodo => {
+      console.log(jsonTodo);
+      this.todo = jsonTodo;
+    });
+  }
+
+  method1() {
+    console.log('Sdsdsd');
+    for (let i = 0; i < this.todo.length; i++) {
+      if (this.todo[i].id === 10) {
+        console.log(this.todo[i]);
+      }
+    }
+  }
+
+  clearList() {
+    this.todo = [];
+    let newDiv1 = document.createElement('p');
+    newDiv1.style.backgroundColor = 'yellow';
+    newDiv1.style.textAlign = 'center';
+    newDiv1.innerText = 'What did you do?';
+    document.body.appendChild(newDiv1);
+  }
 }
